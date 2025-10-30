96d6d4eb9ede6759daeb64c2dcda777d2b6cae24
Fu Xiang <orfuxiang2015@gmail.com>
2025-10-30 14:12:27 +0800

    Allow blank address for edit caretaker

--- src/test/java/seedu/address/logic/parser/EditCaretakerCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/EditCaretakerCommandParserTest.java
@@ -33,7 +33,6 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.EditCaretakerCommand;
 import seedu.address.logic.commands.EditCaretakerCommand.EditCaretakerDescriptor;
-import seedu.address.model.person.Address;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
 import seedu.address.model.person.Relationship;
@@ -78,7 +77,6 @@ public class EditCaretakerCommandParserTest {
         // invalid individual fields
         assertParseFailure(parser, "1" + INVALID_NAME_DESC, Name.INVALID_CHARS);
         assertParseFailure(parser, "1" + INVALID_PHONE_DESC, Phone.INVALID_DIGITS);
-        assertParseFailure(parser, "1" + INVALID_ADDRESS_DESC, Address.BLANK_ADDRESS);
         assertParseFailure(parser, "1" + INVALID_RELATIONSHIP_DESC, Relationship.BLANK_RELATIONSHIP);
 
         // invalid combination
